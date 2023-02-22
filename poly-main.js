7c7c84ca74ff1bd3f94cab09f71ac17b0c62a554
oland2345 <110410860+oland2345@users.noreply.github.com>
2023-02-22 12:36:28 +0100

    Create poly-main.js

---
+++ poly-main.js
@@ -0,0 +1,7 @@
+
+document.getElementById("GaCard").onclick = function(){
+    window.open("nonefind.html", "_self", "Games")
+};
+document.getElementById("TeCard").onclick = function(){
+    window.open("nonefind.html", "_self", "Games")
+};
